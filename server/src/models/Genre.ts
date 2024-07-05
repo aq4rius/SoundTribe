6b6646d0e6dc0499a4466f9e100d2b74a010d508
aq4rius <maksbielecki6@gmail.com>
2024-07-05 16:14:35 +0200

    Create Genre schema and update routing

---
+++ server/src/models/Genre.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IGenre extends Document {
+  name: string;
+  description?: string;
+}
+
+const GenreSchema: Schema = new Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String }
+});
+
+const Genre = mongoose.model<IGenre>('Genre', GenreSchema);
+
+export default Genre;
