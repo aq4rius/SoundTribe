ffe39666519023261f8d424e1f3be812817d0db9
aq4rius <maksbielecki6@gmail.com>
2024-08-09 15:49:05 +0200

    Fix profile edition

--- client/src/components/profile/EditProfile.tsx
+++ client/src/components/profile/EditProfile.tsx
@@ -45,43 +45,43 @@ const EditProfile: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Edit Profile</h2>
+      <h1 className="text-2xl font-semibold text-gray-900 mt-10" >Edit Profile</h1>
       <p>Update your profile information below:</p>
       <br/>
-      <label htmlFor="username">Username:<br/></label>
+      <label className="text-1xl font-semibold text-gray-900 mt-10" htmlFor="username">Username:<br/></label>
       <input
         type="text"
         placeholder="Username"
         value={userInfo.username}
         onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })}
-      /><br/><br/>
-      <label htmlFor="firstName">First Name:<br/></label>
+      /><br/>
+      <label className="text-1xl font-semibold text-gray-900 mt-10" htmlFor="firstName">First Name:<br/></label>
       <input
         type="text"
         placeholder="First Name"
         value={userInfo.firstName}
         onChange={(e) => setUserInfo({ ...userInfo, firstName: e.target.value })}
-      /><br/><br/>
-      <label htmlFor="lastName">Last Name:<br/></label>
+      /><br/>
+      <label className="text-1xl font-semibold text-gray-900 mt-10" htmlFor="lastName">Last Name:<br/></label>
       <input
         type="text"
         placeholder="Last Name"
         value={userInfo.lastName}
         onChange={(e) => setUserInfo({ ...userInfo, lastName: e.target.value })}
-      /><br/><br/>
-      <label htmlFor="location">Location:<br/></label>
+      /><br/>
+      <label className="text-1xl font-semibold text-gray-900 mt-10" htmlFor="location">Location:<br/></label>
       <input
         type="text"
         placeholder="Location"
         value={userInfo.location}
         onChange={(e) => setUserInfo({ ...userInfo, location: e.target.value })}
-      /><br/><br/>
-      <label htmlFor="bio">Bio:<br/></label>
+      /><br/>
+      <label className="text-1xl font-semibold text-gray-900 mt-10"  htmlFor="bio">Bio:<br/></label>
       <textarea
         placeholder="Bio"
         value={userInfo.bio}
         onChange={(e) => setUserInfo({ ...userInfo, bio: e.target.value })}
-      /><br/><br/>
+      /><br/>
       {/* Add more fields for favoriteGenres and preferredContentTypes */}
       <button type="submit">Update Profile</button>
     </form>
